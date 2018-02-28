219d673ea266b651fef527e52b36a6021ad0925a
James <jae8547@truman.edu>
2018-02-28 17:26:18 -0600

    Added manual entry, import, and export for topics

--- abstracts/abstract_frequencies/frequency_analysis.py
+++ abstracts/abstract_frequencies/frequency_analysis.py
@@ -1,10 +1,12 @@
 # James Eason
-# Generate excel file: filename | abstract title | abstract content
 
 import os
 import sys
 from openpyxl import Workbook
 from Tkinter import *
+import Tkinter, Tkconstants, tkFileDialog
+
+### Abstract Retrieval ###
 
 # Remove any non utf-8 characters that might confuse openpyxl
 def cleanse(txt):
@@ -24,22 +26,6 @@ def getFiles(path, files):
                 files.append(newpath)
     return files
     
-# Get topics from topics file
-def getTopics(topicsPath):
-    topics = {}
-    rawContents = cleanse(open(topicsPath, 'r').read()).split('\n')
-    for line in rawContents:
-        line = line.strip()
-        if len(line) < 1 or line[0] == '#':
-            pass
-        else:
-            topicName = line[0:line.index('(')].strip()
-            items = line[line.index('(')+1:line.index(')')].split(',')
-            for x in range(len(items)):
-                items[x] = items[x].strip()
-            topics[topicName] = items
-    return topics
-            
 # Get abstracts from a list of files
 # Returns dict with years as key and array containing abstracts
 def getAbstracts(files):
@@ -52,20 +38,9 @@ def getAbstracts(files):
             abstracts[fileYear] = []
         abstracts[fileYear].append(fileContents)
     return abstracts
-        
-# Get how many times a topic appears in a specified year
-def getTopicCount(topicItems, abstracts):
-    count = 0
-    for abstract in abstracts:
-        abstract = abstract.lower().split()
-        present = False
-        for item in topicItems:
-            if isPresent(item.lower(), abstract):
-                present = True
-        if present:
-            count += 1
-    return count
-    
+
+### Excel Writing ###
+
 # Check if a topic item is present in an abstract
 def isPresent(item, abstract):
     if item in abstract:
@@ -76,16 +51,7 @@ def isPresent(item, abstract):
             if re.match(item, word):
                 return True
     return False
-        
-# Excel column number to string. ex. 1 -> A
-def colNumToString(div):
-    string=""
-    while div>0:
-        module=(div-1)%26
-        string=chr(65+module)+string
-        div=int((div-module)/26)
-    return string
-
+    
 # Generate an excel file based on results    
 def generateExcel(topics, years, results, outputPath):
     wb = Workbook()
@@ -119,16 +85,67 @@ def getCol(ws, topic, size):
         if ws[colNumToString(col) + "1"].value == topic:
             return col
     return -1
+    
+# Excel column number to string. ex. 1 -> A
+def colNumToString(div):
+    string=""
+    while div>0:
+        module=(div-1)%26
+        string=chr(65+module)+string
+        div=int((div-module)/26)
+    return string
+
+### Topics ###
+    
+# Get topics from topics file
+def getTopics(topicsPath):
+    topics = {}
+    rawContents = cleanse(open(topicsPath, 'r').read()).split('\n')
+    for line in rawContents:
+        line = line.strip()
+        if len(line) < 1 or line[0] == '#':
+            pass
+        else:
+            topicName = line[0:line.index('(')].strip()
+            items = line[line.index('(')+1:line.index(')')].split(',')
+            for x in range(len(items)):
+                items[x] = items[x].strip()
+            topics[topicName] = items
+    return topics
+            
+        
+# Get how many times a topic appears in a specified year
+def getTopicCount(topicItems, abstracts):
+    count = 0
+    for abstract in abstracts:
+        abstract = abstract.lower().split()
+        present = False
+        for item in topicItems:
+            if isPresent(item.lower(), abstract):
+                present = True
+        if present:
+            count += 1
+    return count
+    
+# Topics dict -> string                
+def topicsToString(topics):               
+    s = ""
+    i = list(topics.keys())
+    i.sort()
+    for topic in i:
+        s = s + topic + ": " + str(topics[topic]) + "\n"
+    return s
+
+### Main Program and GUI ###
 
 # Run everything
 def run():
     try:
         updateStatus("Running...")
         abstractPath = abstractEntry.get()
-        topicsPath = topicsEntry.get()
         outputPath = outputEntry.get()
-        
-        topics = getTopics(topicsPath)    
+            
+        topics = availableTopics
         files = getFiles(abstractPath, [])
         abstracts = getAbstracts(files)
         updateStatus("Analyzing data...")
@@ -145,29 +162,95 @@ def run():
     except Exception as e:
         updateStatus("Error: " + str(e))
 
-# Topics dict -> string                
-def topicsToString(topics):               
-    s = ""
-    for topic in topics.keys():
-        s = s + topic + ": " + str(topics[topic]) + "\n"
-    return s
-
+# Update status label at bottom of main window
 def updateStatus(status):
     statusLabel.configure(text="Status: " + status)
     w.update_idletasks()
    
-def updateTopicsLabel():
+def addTopicsFromPath(topicsPath):
     try:
-        topicsDict = getTopics(topicsEntry.get())
-        topicsList = topicsToString(topicsDict)
+        topicsDict = getTopics(topicsPath)
+        for topic in topicsDict:
+            availableTopics[topic] = topicsDict[topic]
     except Exception as e:
-        topicsList = "Couldn't load topics. Make sure your path is correct and file is valid."
+        updateStatus("Couldn't load topics. Make sure your path is correct and file is valid.")
         print str(e)
-    topics.configure(text=topicsList)       
+    refreshTopics()    
+
+# Import topics
+def openFileDialog():
+    w.filename = tkFileDialog.askopenfilename(initialdir = os.getcwd(),title = "Select topics file",filetypes = (("text files","*.txt"),("all files","*.*")))
+    if w.filename != None:
+        addTopicsFromPath(w.filename)
+
+# Export topics
+def saveFileDialog():
+    w.filename = tkFileDialog.asksaveasfilename(initialdir = os.getcwd(),title = "Select topics file",filetypes = (("text files","*.txt"),("all files","*.*")))
+    if w.filename != None:
+        writeFile(w.filename)        
+
+# Write exported topics file
+def writeFile(filepath):
+    if not '.txt' in filepath:
+        filepath += '.txt'
+    file = open(filepath, 'w')
+    for topic in availableTopics.keys():
+        s = topic + " ("
+        for item in availableTopics[topic]:
+            s += item + ", "
+        s = s[0:-2] + ")\n"
+        file.write(s)
+    file.close()
+
+# Refresh list of topics
+def refreshTopics():
+    topicsList = topicsToString(availableTopics)
+    topics.configure(text=topicsList)
+
+# Add topic to list from 'New Topic' window
+def addTopic(title, items, t):
+    title = title.get()
+    items = items.get("1.0",'end-1c')
+    items = items.split(',')
+    for x in range(len(items)):
+        items[x] = items[x].strip().encode('ascii','ignore')
+        if len(items[x]) < 1:
+            items.remove(items[x])
+    availableTopics[title] = items 
+    refreshTopics()
+    t.destroy()
+    
+    
+# Create topics
+def newTopicWindow():
+    t = Toplevel(w)
+    t.wm_title("New topic")
+    titleLabel = Label(t, text="Topic title: ")
+    titleLabel.grid(row=0, column=0, sticky=W, pady=5, padx=5)
+    titleEntry = Entry(t, width=50)
+    titleEntry.grid(row=0, column=1, sticky=W, pady=5, padx=5)
+    topicsLabel = Label(t, text="List comma separated topic terms. Use an asterisk to match chunks of words.")
+    topicsLabel.grid(row=1, sticky=W, pady=5, padx=5, columnspan=2)
+    topicsEntry = Text(t, height=5)
+    topicsEntry.grid(row=2, sticky=W, pady=5, padx=5, columnspan=2)
+    Button(t, text='Save', command=lambda: addTopic(titleEntry, topicsEntry, t)).grid(row=3, column=0, sticky=E, pady=5, padx=5)
+    Button(t, text='Quit', command=t.destroy).grid(row=3, column=1, sticky=W, pady=5, padx=5)
+    
+
+availableTopics = {}  
 
 w = Tk()
 w.title("Topic frequency analysis")
 
+Button(w, text='New topic', command=newTopicWindow).grid(row=0, column=0, sticky=W, pady=5, padx=5)
+Button(w, text='Import topics', command=openFileDialog).grid(row=0, column=1, sticky=W, pady=5, padx=5)
+Button(w, text='Export topics', command=saveFileDialog).grid(row=0, column=2, sticky=W, pady=5, padx=5)
+
+topics = Label(w, text="(No topics present)", justify=LEFT)
+topics.grid(row=1, sticky=W, padx=20, pady=10, columnspan=2)
+
+
+"""
 topicsLabel = Label(w, text="Topics file path (.txt file):")
 topicsLabel.grid(row=0, sticky=W, pady=5, padx=5)
 
@@ -178,10 +261,9 @@ topicsEntry.grid(row=0,column=1, sticky=W, pady=5, padx=5)
 topicsHeader = Label(w, text="Topics being used:")
 topicsHeader.grid(row=1, column=0, sticky=W, pady=5, padx=5)
 
-Button(w, text='Load from file', command=updateTopicsLabel).grid(row=1, column=1, sticky=W, pady=5, padx=5)
+Button(w, text='Load from file', command=openFileDialog).grid(row=1, column=1, sticky=W, pady=5, padx=5)
+"""
 
-topics = Label(w, text="(Loaded topics will appear here)", justify=LEFT)
-topics.grid(row=2, sticky=W, padx=20, pady=10, columnspan=2)
 
 
 abstractLabel = Label(w, text="Abstracts path (folder):")
@@ -197,6 +279,7 @@ outputLabel.grid(row=4, sticky=W, pady=5, padx=5)
 outputEntry = Entry(w, width=50)
 outputEntry.insert(END, 'output.xlsx')
 outputEntry.grid(row=4,column=1, sticky=W, pady=5, padx=5)
+
 
 Button(w, text='Generate topic frequencies', command=run).grid(row=5, column=0, sticky=E, pady=5, padx=5)
 Button(w, text='Quit', command=w.quit).grid(row=5, column=1, sticky=W, pady=5, padx=5)
